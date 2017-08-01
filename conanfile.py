5e2a96f25c6618b81a771dd9f0a5ffd411d9d1f9
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:27:48 -0400

    initial commit

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, tools, os
+
+class BoostNumeric_UblasConan(ConanFile):
+    name = "Boost.Numeric_Ublas"
+    version = "1.64.0"
+    generators = "txt"
+    url = "https://github.com/boostorg/ublas"
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    license = "www.boost.org/users/license.html"
+    lib_short_name = "ublas"
+    requires =  "Boost.Concept_Check/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Numeric_Interval/1.64.0@bincrafters/testing", \
+                      "Boost.Range/1.64.0@bincrafters/testing", \
+                      "Boost.Serialization/1.64.0@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Typeof/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
+
+                      #concept_check5 config0 core2 iterator5 mpl5 numeric~interval6 range7 serialization11 smart_ptr4 static_assert1 type_traits3 typeof5 utility5
+                      
+    def source(self):
+        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
+                 .format(self.version, self.url))
+
+    def package(self):
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
+        self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
