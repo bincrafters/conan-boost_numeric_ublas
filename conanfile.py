82bb98420b79410fb8e221e9ffef22d94a76473a
Jerry W <jerrywiltse@gmail.com>
2017-08-15 18:04:08 -0400

    catching up this repo

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,11 @@ from conans import ConanFile, tools, os
 class BoostNumeric_UblasConan(ConanFile):
     name = "Boost.Numeric_Ublas"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/ublas"
+    url = "https://github.com/bincrafters/conan-boost-ublas"
+    source_url = "https://github.com/boostorg/ublas"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "ublas"
+    lib_short_names = ["ublas"]
     requires =  "Boost.Concept_Check/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Core/1.64.0@bincrafters/testing", \
@@ -25,12 +25,14 @@ class BoostNumeric_UblasConan(ConanFile):
                       #concept_check5 config0 core2 iterator5 mpl5 numeric~interval6 range7 serialization11 smart_ptr4 static_assert1 type_traits3 typeof5 utility5
                       
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
 
     def package_id(self):
         self.info.header_only()
