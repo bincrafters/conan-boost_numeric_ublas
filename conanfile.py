890462e478c0668b191df8a20b85068913551ae0
Jerry W <jerrywiltse@gmail.com>
2018-11-17 14:29:45 -0500

    merge in changes from stable branch, mostly paginations from travis

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/stable")
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostNumeric_UblasConan(base.BoostBaseConan):
     name = "boost_numeric_ublas"
