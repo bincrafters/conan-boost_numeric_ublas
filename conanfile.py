6b9a8937c4a4925898d26fe036fd815578afa3fc
Jerry W <jerrywiltse@gmail.com>
2019-01-28 21:38:25 -0500

    add explicit version numbers, add clang 7, fix travis badges, rename .travis dir to .ci, set python 3.6 on travis and appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.69.0@bincrafters/testing")
 
 class BoostNumeric_UblasConan(base.BoostBaseConan):
     name = "boost_numeric_ublas"
+    version = "1.69.0"
     url = "https://github.com/bincrafters/conan-boost_numeric_ublas"
     lib_short_names = ["ublas"]
     header_only_libs = ["ublas"]
