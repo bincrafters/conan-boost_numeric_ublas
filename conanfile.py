c4ab97163dd103ce2766c8fc754e5790dc5c273e
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:06 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,25 +4,10 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostNumeric_UblasConan(base.BoostBaseConan):
     name = "boost_numeric_ublas"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_numeric_ublas"
-    lib_short_names = ["ublas"]
-    header_only_libs = ["ublas"]
-    b2_requires = [
-        "boost_concept_check",
-        "boost_config",
-        "boost_core",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_numeric_interval",
-        "boost_range",
-        "boost_serialization",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_type_traits",
-        "boost_typeof"
-    ]
+    version = "1.70.0"
+    boost_source_repo = {'numeric_ublas': "ublas"}
